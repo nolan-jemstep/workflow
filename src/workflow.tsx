714925d9b96fef4dd92d29dd2f52b3da7a5e6bca
Nolan Pather <nolan@jemstep.com>
2019-07-01 11:22:18 +0200

    run eslint on tsx files

--- src/workflow.tsx
+++ src/workflow.tsx
@@ -1,9 +1,11 @@
-import * as React from "react";
+import * as React from 'react';
 
 interface WorkflowProps {
   name: string;
 }
 
-const Workflow: React.SFC<WorkflowProps> = ({ name }: WorkflowProps): JSX.Element => <div>{`hello ${name}`}</div>;
+const Workflow: React.SFC<WorkflowProps> = ({
+  name,
+}: WorkflowProps): JSX.Element => <div>{`hello ${name}`}</div>;
 
 export default Workflow;
